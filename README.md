91c86d886843157e7ac554de351e51bccc78f126
Chen Yang <chenyang929code@gmail.com>
2018-04-27 17:58:01 +0800

    增加API说明

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 ## 图书借阅管理系统
 
-### API
+### API说明
 | 方法 | 链接 | 说明 | 权限
 | :----: |:---:| :---:| :---: |
-| GET | right-aligned | $1600 | 
-|  | centered | $12 |
-| zebra stripes | are neat | $1 |
+| GET | /user | 获取全部用户信息 | admin | 
+| POST | email=email name=name /user | 新建用户 | admin |
+| GET | /user/1 | 获取某条用户信息 | user admin |
+| POST | user_name=user_name first_name=first_name pw=pw /user/1 | 修改某条用户信息 | admin |
+| GET | /storage | 获取全部图书库存信息 | user admin |
+| POST | book=book /storage | 新书入库 | admin |
+| GET | /storage/1 | 获取某条库存信息 | user admin |
+| POST | book=book inventory=inventory remain=remain /storage/1 | 修改某条库存信息 | admin |
+| GET | /history | 获取借阅记录 | user admin |
+| POST | /history | 新增借阅记录 | user admin |
+| GET | /history/1 | 获取某条借阅记录 | user admin |
+| POST | msg=msg delay=delay /history/1 | 修改某条借阅记录 | user admin |
+
 
 
 
--- api/views.py
+++ api/views.py
@@ -14,8 +14,8 @@ PER = 15   # 分页显示每页数量
 用户API
 GET /user   获取全部用户信息   admin
 POST email=email name=name /user   新建用户   admin
-GET /user/1   获取单条用户信息   user admin
-POST user_name=user_name first_name=first_name pw=pw /user/1   修改单条用户信息   user admin
+GET /user/1   获取某条用户信息   user admin
+POST user_name=user_name first_name=first_name pw=pw /user/1   修改某条用户信息   user admin
 """
 
 
@@ -90,8 +90,8 @@ def user_detail(request, pk):
 库存API
 GET /storage   获取全部图书库存信息   user admin
 POST book=book /storage   新书入库   admin
-GET /storage/1   获取单条库存信息   user admin
-POST /storage/1   修改单条库存信息   admin
+GET /storage/1   获取某条库存信息   user admin
+POST book=book inventory=inventory remain=remain /storage/1   修改某条库存信息   admin
 """
 
 
@@ -168,8 +168,8 @@ def storage_detail(request, pk):
 借阅记录API
 GET /history   获取借阅记录   user admin
 POST /history   新增借阅记录   user admin
-GET /history/1   获取单条借阅记录   user admin
-POST msg=msg delay=delay /history/1   修改单条借阅记录   user admin
+GET /history/1   获取某条借阅记录   user admin
+POST msg=msg delay=delay /history/1   修改某条借阅记录   user admin
 """
 
 
